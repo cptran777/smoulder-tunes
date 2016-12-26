25bda96859715f93f3dfe12819931331fe29fb18
cptran777 <cptran14@gmail.com>
2016-12-26 00:55:51 -0800

    default player to no source and make sure player checks for source before trying to play

--- app/components/listen-player.js
+++ app/components/listen-player.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   didRender() {
-    document.getElementById('player-audio').play();
+    let player = document.getElementById('player-audio');
+    if (player.currentSrc !== '') {
+      player.play();
+    }
   }
 });
--- app/controllers/listen.js
+++ app/controllers/listen.js
@@ -3,14 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   mytest: 'Hello',
   currentSong: {
-    artist: 'Re Zero',
-    title: 'Ending Theme',
-    source: 'http://127.0.0.1:8000/media?name=21643900'
-  },
-  nextSong: {
-    artist: 'FFXIII',
-    title: 'Last Wanderer',
-    source: 'http://127.0.0.1:8000/media?name=10034897'
+    artist: '...',
+    title: '...',
+    source: ''
   },
   songQueue: [],
   actions: {
